d1728165e8db8fce1fe6aa37183cedc66ae88a51
onurhan <onurhanaytac@gmail.com>
2018-01-12 13:22:50 +0300

    poz analizi

--- routes/api.js
+++ routes/api.js
@@ -19,10 +19,59 @@ router.get('/GetWorkItems', function(req, res, next) {
 			return res.send(err);
 		}
 		populate(JSON.parse(JSON.stringify(data)), (err, _data) => {
-			res.send(_data);
+			res.send(analyze(_data));
 		});
 	});
 });
+
+function analyze(data) {
+	var currentLevel = 0;
+	var GlobalData;
+	init(data);
+	function init(data) {
+		GlobalData = data;
+		_.each(data, item => {
+			currentLevel = 0;
+			findLevel(item);
+			item.level = currentLevel;
+		});
+
+		for (var i = 1; i <= currentLevel; i++) {
+			getWorkItemLevel(i, data);
+		}
+	}
+
+	function findLevel(data) {
+		if (!data.subWorkItems.length) {
+			return;
+		}
+		currentLevel++;
+		_.each(data.subWorkItems, workItem => {
+			return findLevel(workItem.workItem);
+		});
+	}
+
+	function getWorkItemLevel(level, data) {
+		_.each(data, item => {
+			if (item.level == level) {
+				findUnitPrice(item);
+			}
+		});
+	}
+
+	function findUnitPrice(item) {
+		if (!item.unitPrice) {
+			item.unitPrice = 0;
+		}
+		_.each(item.subWorkItems, workItem => {
+			if (!workItem.unitPrice) {
+				workItem.unitPrice = _.clone(_.find(GlobalData, {_id: workItem.workItemId}))["unitPrice"];
+			}
+			item.unitPrice += workItem.quantity * workItem.unitPrice;
+		});
+	}
+	return GlobalData;
+}
 
 function populate(_data, callback) {
 	async.each(_data, (item, next) => {
@@ -30,6 +79,11 @@ function populate(_data, callback) {
 			async.each(item.subWorkItems, (subWorkItem, next) => {
 				model.WorkItem.find({_id: subWorkItem.workItemId}, (err, data) => {
 					subWorkItem.workItem = JSON.parse(JSON.stringify(data[0]));
+					subWorkItem["name"] = subWorkItem.workItem["name"];
+					subWorkItem["description"] = subWorkItem.workItem["description"];
+					subWorkItem["unit"] = subWorkItem.workItem["unit"];
+					subWorkItem["unitPrice"] = subWorkItem.workItem["unitPrice"];
+					subWorkItem["subWorkItems"] = subWorkItem.workItem["subWorkItems"];
 					if (subWorkItem.workItem.subWorkItems.length) {
 						subPopulate(subWorkItem.workItem.subWorkItems, (err, _data) => {
 							next();
@@ -51,8 +105,16 @@ function populate(_data, callback) {
 
 function subPopulate(data, callback) {
 	async.each(data, (subWorkItem, next) => {
+		if (!subWorkItem) {
+			return next();
+		}
 		model.WorkItem.find({_id: subWorkItem.workItemId}, (err, data) => {
 			subWorkItem.workItem = JSON.parse(JSON.stringify(data[0]));
+			subWorkItem["name"] = subWorkItem.workItem["name"];
+			subWorkItem["description"] = subWorkItem.workItem["description"];
+			subWorkItem["unit"] = subWorkItem.workItem["unit"];
+			subWorkItem["unitPrice"] = subWorkItem.workItem["unitPrice"];
+			subWorkItem["subWorkItems"] = subWorkItem.workItem["subWorkItems"];
 			if (subWorkItem.workItem.subWorkItems.length) {
 				subPopulate(subWorkItem.workItem.subWorkItems, (err, _data) => {
 					next();
